1c15bca73712a4d3ffd39854df7e7933f5022cae
YeezyYeezyWhatsGood <bcatalfo@gmail.com>
2018-06-04 13:18:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main{
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Pong");
+        JFrame frame = new JFrame("Maze");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
